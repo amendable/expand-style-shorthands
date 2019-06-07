6bd98913f41863c6c582819cf9dd530aa2d1e04f
Domas <domas.bitvinskas@me.com>
2019-06-07 18:07:02 +0300

    v0.0.2: Make sure numbers are numbers (fix flex)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { expandProperty } from 'inline-style-expand-shorthand'
+import _ from 'lodash'
 import defaultMatch from './defaultMatch'
+import transform from './transform'
 
 export default ({ match = defaultMatch } = { match: defaultMatch }) => ({
   match,
   resolve: ({ key, value }) => (
-    expandProperty(key, value)
+    _.reduce(expandProperty(key, value), (memo, val, key) => ({
+      ...memo,
+      [key]: transform(val),
+    }), {})
   ),
 })
---
+++ src/transform.js
@@ -0,0 +1,8 @@
+import _ from 'lodash'
+import isNumber from 'is-number'
+
+export default (val) => {
+  if (!isNumber(val)) return val
+
+  return _.toNumber(val)
+}
