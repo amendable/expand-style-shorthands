95d20ca598c1cb96062a3afca984e4d4fa44a04d
Domas <domas.bitvinskas@me.com>
2020-02-03 18:21:02 +0200

    Ignore objects that might come from other resolvers

--- src/defaultMatch.js
+++ src/defaultMatch.js
@@ -1,14 +1,14 @@
-export default [
-  'border',
-  'borderTop',
-  'borderRight',
-  'borderBottom',
-  'borderLeft',
-  'borderWidth',
-  'borderStyle',
-  'borderColor',
-  'padding',
-  'margin',
-  'outline',
-  'flex',
-]
+import _ from 'lodash'
+import isNumber from 'is-number'
+import defaultMatchKeys from './defaultMatchKeys'
+
+export default ({
+  key,
+  value,
+}) => {
+  if (!_.includes(defaultMatchKeys, key)) return false
+  if (_.isString(value)) return true
+  if (isNumber(value)) return true
+
+  return false
+}
