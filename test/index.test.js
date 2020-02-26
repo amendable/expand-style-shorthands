765fc096da9bf6782b5c4eb0da7f2bcc7c66e7d4
Justas Palumickas <jpalumickas@gmail.com>
2020-02-26 19:21:10 +0200

    Add simple test

---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+import shorthandsResolver from '../src';
+
+test('returns correct value on flex 1', () => {
+  const resolver = shorthandsResolver();
+  expect(resolver.resolve({ key: 'flex', value: 1 })).toEqual({
+    flexGrow: 1,
+  });
+})
+
+test('does not match when flex is excluded', () => {
+  const keys = (defaultKeys) => defaultKeys.filter(k => k !== 'flex');
+  const resolver = shorthandsResolver({ keys });
+
+  expect(resolver.match({ key: 'flex', value: 1 })).toEqual(false);
+})
