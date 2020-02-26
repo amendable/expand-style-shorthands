7cd2fd914331f5267bb4aaea391cae8dad17d82f
Justas Palumickas <jpalumickas@gmail.com>
2020-02-26 19:20:53 +0200

    Use microbundle to build

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,6 @@
 module.exports = {
-  "presets": [
-    "@babel/preset-env",
-    "@babel/preset-react"
-  ],
-  plugins: [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-proposal-class-properties"
-  ],
-  "env": {
-    "esm": {
-      "presets": [["@babel/preset-env", { "modules": false }]],
-      "plugins": [["@babel/plugin-transform-runtime", { "useESModules": true }]]
-    }
-  }
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ]
 }
